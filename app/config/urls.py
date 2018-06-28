985d74361bedfb2ffa2dba3dd25c4004d6f6f122
Ahyeon <ahyeoncode@gmail.com>
2018-06-29 02:31:12 +0900

    index view구현 및 자동으로 posts:post-list로 redirect되도록 설정

--- app/config/urls.py
+++ app/config/urls.py
@@ -18,7 +18,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls')),
     # path('media/<str:path>/', 특정view_function),
