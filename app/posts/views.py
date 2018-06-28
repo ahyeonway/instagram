aa140c72e5cb311a36c865e3d0ca57dd991f7db1
Ahyeon <ahyeoncode@gmail.com>
2018-06-29 02:06:02 +0900

    post_lilist, post_detail에서 template사용

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def post_list(request):
+    return render(request, 'posts/post_list.html')
+
+
+def post_detail(request, pk):
+    return render(request, 'posts/post_detail.html')
